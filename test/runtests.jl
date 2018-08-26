102e6aa10d2e71fd1a4cc2b786493a5e21ad1c60
mbrueckner <matthias.brueckner@posteo.de>
2018-08-26 13:07:33 +0100

    update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ t = Tuner(0)
 @test SplineHazard.get_tune_param(t) == 2.38
 @test (t.cur_blk == 1) & (t.cur_iter == 0)
 update!(t, 1, true)
-@test (t.cur_blk == 1) & (t.cur_iter == 1)
+@test (t.cur_blk == 1) & (t.cur_iter == 0)
 
 t = Tuner(1000)
 @test SplineHazard.get_tune_param(t) == 2.38
